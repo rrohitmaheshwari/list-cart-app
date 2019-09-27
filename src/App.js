5cfdb78bcedae818f659d5667dd573dc28daf46f
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2019-09-27 00:15:53 -0700

    Added basic structure for list app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 import List from './components/List.js'
 import Cart from './components/Cart.js'
 
-function App() {
-    return (
-        <div className="App">
-            <div>
-                List Cart App
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state={sourceList : ["Item1","Item2","Item3","Item4"]};
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    List Cart App
+                </div>
+                <div>
+                    <List sourceList={this.state.sourceList}/>
+                    <Cart/>
+                </div>
             </div>
-            <div>
-                <List/>
-                <Cart/>
-            </div>
-        </div>
-    );
+        );
+    }
 }
+
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Lists from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-function List() {
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(3, 2),
+    },
+}));
+
+function List(props) {
+    const classes = useStyles();
+    let {sourceList} = props;
+    console.log(sourceList);
     return (
         <div className="List">
-            List Component
+            <Paper className={classes.root}>
+                <Typography variant="h5" component="h3">
+                    List
+                </Typography>
+                <Divider/>
+                <Lists component="nav" aria-label="mailbox folders">
+                    {sourceList.map((data, index) =>
+                        <div key={index}>
+                            <ListItem button>
+                                <ListItemText primary={data}/>
+                            </ListItem>
+                            <Divider light/>
+                        </div>
+                    )}
+                </Lists>
+            </Paper>
         </div>
     );
 }
